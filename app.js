768a3285954c72ec7c6ff184a2f4dba0a1fa6f56
Karina Amador <akarina.at@gmail.com>
2021-07-26 18:31:39 -0500

    examen resuelto 1

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const express = require('express');
+var PORT = process.env.PORT || 5000;
 // ## Agrega la dependencia de express ##
 
 
@@ -27,12 +29,13 @@ var animals = [
 
 // ## Inicializa express ##
 
-
+const app = express();
 // ## Inicializa el motor de plantillas con EJS ##
-
+app.set('view engine', 'ejs');
 
 // ## Agrega el middleware de express para que el servidor soporte json ##
 
+app.use(express.urlencoded({extended:false}));
 
 /* ############## RUTAS ################  */
 
@@ -43,6 +46,11 @@ var animals = [
       b) animals: con referencia al arreglo animals. 
 */
 
+app.get('/all-pets',(req,res) => {
+console.log('all-pets');
+res.render('pages/all-pets', {title: 'All', animals: animals})
+})
+
 
 /* (2)  Crea una ruta POST que: 
    - escuche en /api/addAnimal 
@@ -50,6 +58,12 @@ var animals = [
    - lo agregue al arreglo animals
 
 */
+
+app.post('/api/addAnimal', (req,res) => {
+  var newAnimal = req.body;
+  animals.push(newAnimal);
+
+})
  
 /* (3)  Crea una ruta GET que: 
    - escuche en /dog  
@@ -57,6 +71,10 @@ var animals = [
       a) title:  con el valor "Dog" 
       b) animals: con el valor del indice[0]
 */ 
+
+app.get('/dog', (req,res) => {
+  res.render('pages/dog',{title: 'Dog', dog: animals[0]})
+})
 
 
 /* (4)  Crea una ruta GET que: 
@@ -68,9 +86,29 @@ var animals = [
       a) title:  con el valor obtenido de la ruta dinámica
       b) animal: con la variable que almacena el objeto encontrado. Si no lo encuentra la variable se va vacía
 */ 
+
+app.get('/api/getAnimal/:animal', (req,res) => {
+
+  var _title = req.params.title;
+  var _animal = req.params.animal;
+  var response;
+
+  for(var i = 0; i++ < animals.length; i++){
+    if(_animal === animals[i].animalType){
+     return res.json(animals[i]);
+    } else if (_animal === !animals[i].animalType){
+      return res.json(animals[i]);
+    }
+  } 
+
+  res.render('pages/any', {title: _title, animal: _animal})
+
+})
    
 
 
 //  Agrega el código necesario para que el servidor escuche en el puerto 5000
-
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
 
